b78c9232031ad32b11614a1e739766b83d784854
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-16 14:48:35 +0800

    add timer

--- script.js
+++ script.js
@@ -41,6 +41,17 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
+    var count = 15;
+    var interval = setInterval(function() {
+        document.getElementById('count').innerHTML = count;
+        count--;
+        if (count === 0) {
+            clearInterval(interval);
+            document.getElementById('count').innerHTML = 'Done';
+            // or...
+            alert("You're out of time!");
+        }
+    }, 1000);
     questionCounter = 0;
     score = 0;
     availableQuesions = [...questions];
